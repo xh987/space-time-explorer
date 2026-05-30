cdb71f2ba17e59cc0255491edb3ed201b704ee4c
xh987 <148248554@qq.com>
2026-05-30 16:01:36 +0800

    fix: 改用hash路由模式解决404问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Game from '../views/Game.vue'
 import Result from '../views/Result.vue'
@@ -56,7 +56,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory('/space1/'),
+  history: createWebHashHistory(),
   routes
 })
 
